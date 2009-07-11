d2fae6960924a8343c14308917a6a07979ed62bc
CraigGiles9 <CraigGiles9@ba273066-68f3-11de-9593-3d8b33745fea>
2009-07-11 17:02:11 +0000

    Removed most opengl setup calls from the screen controller. When rendering a texture to the screen, the Texture2D class will handle all that information.

--- trunk/CPP Classes/Screen Manager/ScreenController.cpp
+++ trunk/CPP Classes/Screen Manager/ScreenController.cpp
@@ -16,7 +16,7 @@ ScreenController* ScreenController::getInstance()
 }
 
 #pragma mark Constructor(s) / Destructor
-
+#pragma mark ----------
 ScreenController::ScreenController() :
 	LANDSCAPE_MODE(true),
 	TRANSITION_ON_TIME(.70f),
@@ -35,19 +35,16 @@ ScreenController::~ScreenController()
 }
 
 #pragma mark Setup view and Load Content
-
-//
+#pragma mark ----------
 //	This function sets up OpenGL for how we wish to draw objects to the 
 //	screen by editing the specific "modes" such as the projection
 //	and model view matricies. 
-//
 void ScreenController::setupView()
 {	
 	//	Modify the Projection matrix
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	
-	//
 	//	Orthof projection is used for 2d games. This sets the coordinates to
 	//	(0, 0) at the top left corner of the screen, and as you move downward
 	//	your y value will increase. As you move to the right, your x value will
@@ -55,7 +52,7 @@ void ScreenController::setupView()
 	//	(left, right, bottom, top, near, far)
 	//
 	//	If the game is going to be played in landscape mode, enable
-	//	it via the bool switch at the top of the GLViewController.m file.
+	//	it via the bool switch at the top of the ScreenController file.
 	if ( LANDSCAPE_MODE )
 	{
 		viewport = Rectangle(0, 0, 480, 320);
@@ -71,80 +68,36 @@ void ScreenController::setupView()
 		glOrthof(0.0, viewport.width, viewport.height, 0.0, -1.0, 1.0);	
 	}
 	
-	
-	//
-	//	Setup Model view matrix
-	//	Load graphics settings
-	//
 	glMatrixMode(GL_MODELVIEW);
-	
-	glDisable(GL_DEPTH_TEST);
-	glBlendFunc(GL_ONE, GL_SRC_COLOR);
-	glEnable(GL_BLEND);	
-	
-	//	needed to draw textures using Texture2D
-	glEnable(GL_TEXTURE_2D);	
-	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-	glEnableClientState(GL_VERTEX_ARRAY);
-	
-	
-	//	enables alpha for transparent textures
-	//	I forget where I got these commands, iDevGames.net I think
-	glAlphaFunc(GL_GREATER, 0.1f);
-	glEnable(GL_ALPHA_TEST);
-	
-	//
+	glLoadIdentity(); 
+
 	//	Setup clear color (cornflower blue'ish)
 	//	Call me crazy, but I got so used to this color developing
 	//	for XNA. This is a little nod to the Microsoft Development
 	//	Team :)
-	//
-	glLoadIdentity(); 
 	glClearColor(.39f, 0.58f, 0.920f, 0.0f); 
 }
 
-//
-//	Now that the view has been initialized and OpenGL settings have been
-//	modified, it is time to load enough content of the game to get started.
-//
 void ScreenController::loadContent()
-{	
-	//
-	//	Seed the random number generator so we do not need to seed it 
-	//	again
-	//
-	RNG::seedRandomNumberGenerator();
-	
-	//
-	//	Since I am using dynamically allocated lists, we need to allocate
-	//	memory for the lists. 
-	//
+{
 	screens = new list<GameScreen *>;
 	screensToUpdate = new list<GameScreen *>;
-	
-	//
-	//	cycle through each screen in the screens list and tell them to
-	//	load their content
-	//	
+
+	//	Cycle through all screens and instruct them to load their content
 	for ( screen = screens->begin(); screen != screens->end(); ++screen)
-	{
 		(*screen)->loadContent();
-	}
+
 	
-	//
 	//	Allocate memory for the input manager and the blankTexture used
 	//	to fade the screen in / out.
-	//
 	input = new InputController( LANDSCAPE_MODE );
 	blankTexture = Textures->getTexture( "blankTexture" );
 	blankTexture->addReference();
 
-	//
-	//	Once we are initialized, set the bool values to appropriate values. 
-	//
 	isInitialized = true;
 	traceEnabled = false;
-	
+
+#warning TitleScreen to the Screen Controller
 	//
 	//	Adds a title screen to the game stack. This will be taken out 
 	//	later, and right now is only used for debugging purposes. It
@@ -158,7 +111,7 @@ void ScreenController::loadContent()
 }
 
 #pragma mark Update / Draw Screens
-
+#pragma mark ----------
 //
 //	Cycles through each game screen in the screens vector and calls
 //	their update method. If it is determined that the screen is
@@ -188,7 +141,6 @@ void ScreenController::updateView(float deltaTime)
 	//	any updates needed
 	for (screen = screensToUpdate->begin(); screen != screensToUpdate->end(); ++screen)
 	{
-		//update the screen
 		(*screen)->update( deltaTime, otherScreenHasFocus, coveredByOtherScreen );
 		
 		if ( (*screen)->getScreenState() == TransitionOn ||
@@ -211,20 +163,12 @@ void ScreenController::updateView(float deltaTime)
 		traceScreens();
 }
 
-//
-//	draws a list of the active screens for debug purposes
-//
+
 void ScreenController::traceScreens() const
 {
 #warning Implement this function (ScreenController::traceScreens)
 }
 
-//
-//	Cycles through each game screen in the screens vector and calls
-//	their draw method. Since every game screen needs to be drawn and 
-//	it is possible to draw some screens on top of other screens,
-//	the screens are drawn from "bottom to top."
-//
 void ScreenController::drawView(float deltaTime) const
 {	
 	list<GameScreen *>::iterator drawScreen;
@@ -234,15 +178,14 @@ void ScreenController::drawView(float deltaTime) const
 }
 
 #pragma mark Adding / Removing a screen from the controller
+#pragma mark ----------
 
-//
-//	when adding a screen to the screen manager, we will be
-//	setting some default values for the screen and then
-//	calling the screens "loadContent" function. Once everything
-//	is loaded, the screen will be added to the screens list.
-//
 void ScreenController::addScreen ( GameScreen *gameScreen )
 {	
+	//	when adding a screen to the screen manager, we will be
+	//	setting some default values for the screen and then
+	//	calling the screens "loadContent" function. Once everything
+	//	is loaded, the screen will be added to the screens list.
 	gameScreen->setViewport( viewport );
 	gameScreen->setTransitionOnTime( TRANSITION_ON_TIME );
 	gameScreen->setTransitionOffTime( TRANSITION_OFF_TIME );
@@ -253,39 +196,31 @@ void ScreenController::addScreen ( GameScreen *gameScreen )
 	screens->push_back( gameScreen );
 }
 
-//
-//	Removes a screen from the game screens vector and cleans up
-//	any memory associated with the screen by calling the screens
-//	unloadContent function, and deleting the pointer associated
-//	with the screen. This function will instantly kill a screen
-//	so it is recommended that if the user wishes a screen to exit
-//	gracefully, the client should call the screens "exit()" function.
-//
 void ScreenController::killScreen( GameScreen *gameScreen )
 {
-	//	unload the screens data
+	//	Removes a screen from the game screens vector and cleans up
+	//	any memory associated with the screen by calling the screens
+	//	unloadContent function, and deleting the pointer associated
+	//	with the screen. This function will instantly kill a screen
+	//	so it is recommended that if the user wishes a screen to exit
+	//	gracefully, the client should call the screens "exit()" function.
 	gameScreen->unloadContent();
-	
-	//	remove the screen from the screens array
 	screens->remove( gameScreen );
 	screensToUpdate->remove( gameScreen );
 	
-	//	delete the screens memory
 	delete gameScreen;
 }
 
 #pragma mark Transition Functions
+#pragma mark ----------
 
-//
-//	Helper method to draw a translecent black fullscreen sprite, used
-//	for fading screens in and out, and for darkening the background
-//	behind pop up screens.
-//
 void ScreenController::fadeBackBufferToBlack( float alpha ) const
 {
-	glColor4f(alpha,alpha,alpha,alpha);
-	blankTexture->draw( viewport );
-	glColor4f(1, 1, 1, 1);	
+#warning re-work this system
+	//	Helper method to draw a translecent black fullscreen sprite, used
+	//	for fading screens in and out, and for darkening the background
+	//	behind pop up screens.
+	blankTexture->draw(viewport, Color(alpha, alpha, alpha, alpha));
 }
 
 
